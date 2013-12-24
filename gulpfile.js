082eb38feff42542381114dadaa636213029fc2b
Ramesh Nair <ram@hiddentao.com>
2013-12-24 08:01:17 +0800

    Test failures now break the build.

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,12 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 
 gulp.task('tests', function () {
-  return gulp.src('test.js')
+  gulp.src('test.js')
     .pipe(mocha({
       ui: 'bdd',
       reporter: 'spec'
-    }))
+    }));
 });
 
 
 gulp.task('default', ['tests'], function() {});
-
-
